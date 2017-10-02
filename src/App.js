38be3ed3fa5fa69de2fe44270c39d41b115581d4
katy lavallee <katylava@gmail.com>
2017-10-02 14:00:17 -0500

    Abstract out bio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import me from './me.png'
 import './App.css';
+
+const bio = "a skeptical, libertarian, day-dreamin', cheese-eatin', scifi-watchin', TV-obsessin', guitar-plinkin', pet-spoilin', python-codin' engineer <a href=\"https://twitter.com/OReillyMedia\">@OReillyMedia</a>"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Bio
+          portrait={me}
+          name="katy lavallee"
+          bio="a girl has no bio"
+        />
+      </div>
+    );
+  }
+}
+
+class Bio extends Component {
+  render() {
+    return (
+      <div classname="App-bio">
         <header className="App-header">
-          <img src={me} className="App-logo" alt="logo" />
-          <h1 className="App-title">katy lavallee</h1>
+          <img src={this.props.portrait} className="App-logo" alt={`portrait for ${this.props.name}`} />
+          <h1 className="App-title">{this.props.name}</h1>
         </header>
         <p className="App-intro">
-          a skeptical, libertarian, day-dreamin', cheese-eatin', scifi-watchin', TV-obsessin', guitar-plinkin',
-          pet-spoilin', python-codin' engineer <a href="https://twitter.com/OReillyMedia">@OReillyMedia</a>
+          {this.props.bio}
         </p>
       </div>
     );
