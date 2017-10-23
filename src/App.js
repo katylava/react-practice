db007ef4ba9a3efacac757edc408125e27b73a19
katy lavallee <katylava@gmail.com>
2017-10-23 11:36:31 -0500

    Use bullets for bio

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import me from './me.png'
 import './App.css';
 import Bio from './Bio.js';
 
-const bio = "a skeptical, libertarian, day-dreamin', cheese-eatin', scifi-watchin', TV-obsessin', guitar-plinkin', pet-spoilin', python-codin' engineer <a href=\"https://twitter.com/OReillyMedia\">@OReillyMedia</a>"
+// const bio = "a skeptical, libertarian, day-dreamin', cheese-eatin', scifi-watchin', TV-obsessin', guitar-plinkin', pet-spoilin', python-codin' engineer <a href=\"https://twitter.com/OReillyMedia\">@OReillyMedia</a>"
 
 class App extends Component {
   render() {
+    let bullets = [
+      'a girl has no bio',
+      'i don\'t know who i am'
+    ];
     return (
       <div className="App">
         <Bio
           portrait={me}
           name="katy lavallee"
-          bio="a girl has no bio"
+          bullets={bullets}
         />
       </div>
     );
--- src/Bio.js
+++ src/Bio.js
@@ -3,16 +3,24 @@ import React, { Component } from 'react';
 class Bio extends Component {
   render() {
     return (
-      <div classname="App-bio">
+      <div className="App-bio">
         <header className="App-header">
           <img src={this.props.portrait} className="App-logo" alt={`portrait for ${this.props.name}`} />
           <h1 className="App-title">{this.props.name}</h1>
         </header>
-        <p className="App-intro">
-          {this.props.bio}
-        </p>
+        <div className="App-intro">
+          <ul>
+            {this.makeBullets()}
+          </ul>
+        </div>
       </div>
     );
+  }
+
+  makeBullets() {
+    return this.props.bullets.map((bullet, index) => {
+        return <li key={index}>{bullet}</li>;
+    });
   }
 }
 
